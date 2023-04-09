6972486650056011cba44a335886f0db799dc027
Sarwar Jahan Shohan <sarwar.shohan24@gmail.com>
2023-04-09 23:03:03 +0600

    make all component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Header from './component/Header';
+import TodoList from './component/TodoList';
+import Footer from './component/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <div
+            class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Navbar />
+
+
+            <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+                {/* header */}
+                <Header />
+                <hr class="mt-4" />
+
+                {/* todo list */}
+                <TodoList />
+
+                <hr class="mt-4" />
+
+                {/* footer */}
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Todo from './Todo'
+
+function TodoList() {
+    return (
+        <div
+            class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto"
+        >
+
+
+
+            <Todo />
+
+
+        </div>
+    )
+}
+
+export default TodoList
